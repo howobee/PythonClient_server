2873a4c8b02a753b01969ce0d44428a22d24753b
howobee <howobee@gmail.com>
2019-11-30 14:46:54 +0300

    initial commit

---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://github.com/howobee/PythonClient_server.git
---
+++ server.py
@@ -0,0 +1,46 @@
+import socket
+import time
+
+#Получаем порт и ip нашего сервера
+host = socket.gethostbyname(socket.gethostname())
+port = 9090
+
+# Создаем пустой список для будущих клиентов
+clients = []
+
+#Создаем сокет используя Ip4 и протокол UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+
+quit = False
+
+print("<Server Started>")
+
+#============================ЦИКЛ СЕРВЕРА===================================#
+
+while not quit:
+	try:
+		data, addr = s.recvfrom(1024)
+		
+		# Проверка содержиться ли клиент в списке Clients если нет тогда добавляем
+		if addr not in clients:
+			clients.append(addr)
+		
+		#Получения текущего времени и сохранения его в переменной itsatime	
+		itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+		
+		# Отправления отформатированного сообщения всем клиентам кроме отправителя
+		print("["+addr[0]+"]=["+str(addr[1])+"]=["+itsatime+"]/",end="")
+		print(data.decode("utf-8"))
+		
+		#Проверка перед отправкой сообщения
+		for client in clients:
+			if addr != client:
+				s.sendto(data, client)
+		
+	except:
+		print("\n<Server Stopped>")
+		quit = True
+		
+# Закрываем соке
+s.close()
